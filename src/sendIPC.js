482e6f964bf545124e0a442ee780f125cee21c1a
Anshul Khope <anshul@iocare.in>
2021-10-19 16:28:58 +0530

    Create sendIPC.js : Created separate file for ipcRenderer events

---
+++ src/sendIPC.js
@@ -0,0 +1,14 @@
+const $ = require('jquery');
+const { ipcRenderer } = require('electron');
+
+$('#close-w').on('click', function() {
+    ipcRenderer.send('closed', true);
+});
+$('#minimz-w').on('click', function() {
+    ipcRenderer.send('minimized', true);
+});
+$('#slideDo').on('click', function() {
+    handleDB();
+});
+
+$('#currentFeature').html('View Chrome History');
