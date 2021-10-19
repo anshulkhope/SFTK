bc942c6e895d631b7ba63dc86335530485e3672e
Anshul Khope <anshul@iocare.in>
2021-10-19 16:05:29 +0530

    Update main.js : Added support to load history on Linux OS

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ const $ = require('jquery');
 const { ipcRenderer } = require('electron');
 const path = require('path');
 const sqlite3 = require('sqlite3');
+const os = require('os')
 
-const appData = process.env.LOCALAPPDATA;
-const dbPath = path.join(appData, 'Google/Chrome/User Data/Default/History');
+
+let dbPathBasedOnOS;
+if (process.platform === 'win32') {
+    const appData = process.env.LOCALAPPDATA;
+    dbPathBasedOnOS = path.join(appData, 'Google/Chrome/User Data/Default/History');
+}
+if (process.platform === 'linux') {
+    dbPathBasedOnOS = '/home/' + os.userInfo().username + '/.config/google-chrome/Default/History';
+}
 
 function handleLoadOverlay() {
     $('#overlay').addClass('animate__animated');
@@ -35,12 +43,13 @@ function appendRowToActivityList(id, title, url) {
 
 function handleDB() {
     console.log('Opening Chrome history Database...');
-    const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+    console.log('Finding Database on path: ' + dbPathBasedOnOS);
+    const db = new sqlite3.Database(dbPathBasedOnOS, sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             alert(err.message);
             return;
         }
-        console.log('Opened chrome history database from path: ' + dbPath);
+        console.log('Opened chrome history database from path: ' + dbPathBasedOnOS);
     });
     db.serialize(() => {
         db.each('SELECT id,title,url FROM urls', (err, row) => {
